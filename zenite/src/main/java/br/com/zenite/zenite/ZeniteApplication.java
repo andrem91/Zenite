68c668f4f8df25a4d367a0b2321cd92fca14dc24
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-07-29 10:46:30 -0300

    criando o projeto spring

---
+++ zenite/src/main/java/br/com/zenite/zenite/ZeniteApplication.java
@@ -0,0 +1,12 @@
+package br.com.zenite.zenite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeniteApplication.class, args);
+	}
+}
---
+++ zenite/src/main/java/br/com/zenite/zenite/model/CategoriaModel.java
@@ -0,0 +1,38 @@
+package br.com.zenite.zenite.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categoria")
+public class CategoriaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank
+	@Size(min = 2, max = 50)
+	private String categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
