ce7ad0dec5b8ef551e5f0d10d9c3a6ea65d9fdff
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-08-03 09:20:14 -0300

    Criando o repository e o corpo do controller

---
+++ zenite/src/main/java/br/com/zenite/zenite/controller/CategoriaController.java
@@ -0,0 +1,17 @@
+package br.com.zenite.zenite.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zenite.zenite.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository repository;
+}
