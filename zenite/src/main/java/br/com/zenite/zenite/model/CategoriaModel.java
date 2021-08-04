5a4708bed238cc859d989befcaf6252c29a73487
andrem91 <andrem166@gmail.com>
2021-08-04 10:59:06 -0300

    criando a Model postagem e relacionando as tabelas

--- zenite/src/main/java/br/com/zenite/zenite/model/CategoriaModel.java
+++ zenite/src/main/java/br/com/zenite/zenite/model/CategoriaModel.java
@@ -1,12 +1,18 @@
 package br.com.zenite.zenite.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -19,6 +25,10 @@ public class CategoriaModel {
 	@NotBlank
 	@Size(min = 2, max = 50)
 	private String categoria;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<PostagemModel> postagem;
 
 	public long getId() {
 		return id;
@@ -35,4 +45,13 @@ public class CategoriaModel {
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
+
+	public List<PostagemModel> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<PostagemModel> postagem) {
+		this.postagem = postagem;
+	}
+
 }
