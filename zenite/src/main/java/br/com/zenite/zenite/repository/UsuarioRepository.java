1f376c7b608a69e3b9c57aa4818344507be58b9b
jaquecantarino <jaquelinecantarino@outlook.com>
2021-08-10 12:30:30 -0300

    Refatorando Código

--- zenite/src/main/java/br/com/zenite/zenite/repository/UsuarioRepository.java
+++ zenite/src/main/java/br/com/zenite/zenite/repository/UsuarioRepository.java
@@ -13,5 +13,5 @@ public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
 	
 	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
 	
-	public Optional<UsuarioModel> findByUsuario(String usuario);
+	public Optional<UsuarioModel> findByEmail(String usuario);
 }
